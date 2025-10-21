81c955de83693b7ecc669c97d58ffdbff6d3d678
João Vitor <joao789roye@gmail.com>
2025-10-20 23:47:26 -0300

    feat(formulario): adicionando model para formulario

--- src/models/Caso.ts
+++ src/models/Caso.ts
@@ -1,11 +1,14 @@
 import { Anexo } from "./Anexo";
+import { Formulario } from "./Formulario";
 
 export class Caso {
     private protocoloCaso?: number;
     private anexos: Anexo[] = [];
+    private formulario: Formulario;
     
-    constructor(protocoloCaso: number) {
+    constructor(protocoloCaso: number, formulario: Formulario) {
         this.protocoloCaso = protocoloCaso;
+        this.formulario = formulario;
     }
 
     // Getters
@@ -20,4 +23,14 @@ export class Caso {
     public getAnexoById(idAnexo: number): Anexo | undefined{
         return this.anexos.find(anexos => anexos.getidAnexo() === idAnexo);
     }
+
+    //Setters
+    public setAnexos(anexos: Anexo[]) {
+        this.anexos = anexos;
+    }
+
+    public setProtocoloCaso(protocoloCaso: number) {
+        this.protocoloCaso = protocoloCaso;
+    }
+
 }
