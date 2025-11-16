6e1cbcb957466c892fbd098353c13a120bd8b6c3
CarlaCristinaSA <carlacristina130523@gmail.com>
2025-11-16 12:14:52 -0300

    feat(tela-configuracoes-conta): adicionar modal de atualização de cargo

--- src/view/tela-configuracoes-conta/script.js
+++ src/view/tela-configuracoes-conta/script.js
@@ -2,6 +2,10 @@ const modalNome = document.getElementById('modalNome');
 const botaoNome = document.getElementById('itemNome');
 const botaoFechar = document.querySelector('.modal-close');
 
+const modalCargo = document.getElementById('modalCargo');
+const botaoCargo = document.getElementById('itemCargo');
+const botaoFecharCargo = document.querySelectorAll('.modal-close')[1];
+
 botaoNome.addEventListener('click', () => {
     modalNome.classList.add('visible');
 });
@@ -10,8 +14,19 @@ botaoFechar.addEventListener('click', () => {
     modalNome.classList.remove('visible');
 });
 
+botaoCargo.addEventListener('click', () => {
+    modalCargo.classList.add('visible');
+});
+
+botaoFecharCargo.addEventListener('click', () => {
+    modalCargo.classList.remove('visible');
+});
+
 window.addEventListener('click', (evento) => {
     if (evento.target === modalNome) {
         modalNome.classList.remove('visible');
+    }
+    if (evento.target === modalCargo) {
+        modalCargo.classList.remove('visible');
     }
 });
